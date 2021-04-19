c93801f7ca9e1e735f2f28abe851686d34dda374
mahmoud.benayed@esprit.tn <mahmoud.benayed@esprit.tn>
2021-04-19 16:42:37 +0200

    retrieve all commit

--- src/main/java/edu/esprit/immo/Dari/controller/AppointmentController.java
+++ src/main/java/edu/esprit/immo/Dari/controller/AppointmentController.java
@@ -1,7 +1,10 @@
 package edu.esprit.immo.Dari.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -35,5 +38,14 @@ public class AppointmentController {
 	public void deleteApp (@PathVariable("appId") String idAppt)
 	{
 		sapp.deleteApp(idAppt);
+		
 	}
-}
+	
+	
+	@GetMapping("/retrieve-all-appointment")
+	public List<Appointment> getAppointments() {
+	List<Appointment> list = sapp.retrieveAllApp();
+	return list;
+	
+	}
+	}
