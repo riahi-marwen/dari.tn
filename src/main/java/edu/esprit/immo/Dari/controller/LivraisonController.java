4bd090b6498b4edde04b809a28fdc8b76d0c0bd5
mahmoud.benayed@esprit.tn <mahmoud.benayed@esprit.tn>
2021-04-21 00:16:29 +0200

    Controller Livraison effectué

--- src/main/java/edu/esprit/immo/Dari/controller/LivraisonController.java
+++ src/main/java/edu/esprit/immo/Dari/controller/LivraisonController.java
@@ -1,7 +1,13 @@
 package edu.esprit.immo.Dari.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +28,33 @@ public class LivraisonController {
 		sliv.addLivraison(liv);
 		
 	}
+	
+	@PutMapping("updateLivraison")
+	public void updateLivraison (@RequestBody Livraison liv) {
+		sliv.updateLivraison(liv);
+	}
 
+	@DeleteMapping ("/deleteLivraison/{livId}")
+	public void deleteLivraison (@PathVariable ("livId") String idLivraison ) {
+		sliv.deleteLivraison(idLivraison);
+	}
+	
+	@GetMapping("/retrieve-all-livraison")
+	public List<Livraison> getLivraisons (){
+		List<Livraison> list =sliv.retrieveAllLivraison();
+		return list ;
+		
+	}
+	
+	@GetMapping ("/retrieve-livraison/{livId}")
+	public Livraison retrieveLivraison (@PathVariable ("livId") String idLivraison ) {
+	
+		return sliv.retrievLiv(idLivraison); 
+	}
+	 
+	
+	
 }
+
+	
+	
