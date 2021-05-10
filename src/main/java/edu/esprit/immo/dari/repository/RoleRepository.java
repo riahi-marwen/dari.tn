62b35b24f8ffd209a2fc5061ba657f4ea36c3b72
riadhamami <riadh.amami@esprit.tn>
2021-05-10 09:55:06 +0100

    changing User to work with jsf

--- src/main/java/edu/esprit/immo/dari/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.esprit.immo.dari.repository;
-
-import edu.esprit.immo.dari.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
--- src/main/java/edu/esprit/immo/dari/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.esprit.immo.dari.repository;
-
-import edu.esprit.immo.dari.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String username);
-}
